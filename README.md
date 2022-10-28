59637d8f2886d3ee1757fd7d956470290acf1d8b
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2022-10-28 13:31:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Request
+
+curl <http://localhost:8000/?max=100&min=10&limit=10>
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+function getRandomArbitrary(min = 0, max = 100) {
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+app.get('/', (req, res) => {
+  const { max, min, limit } = req.query
+
+  const data = []
+  const _limit = limit || 10
+
+  for (let i = 0; i < _limit; i++) {
+    const ran = getRandomArbitrary(parseInt(min), parseInt(max))
+    data.push(ran.toString())
+  }
+
+  res.setHeader('Content-Type', 'text/plain');
+
+  return res.send(`${[...data]}`.replace(/,/g, ' '))
+})
+
+
+app.listen(8000, () => console.log('Run...'))
