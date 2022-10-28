0e9ec712cd9146a3807287378c2ff1924b68e0db
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2022-10-28 13:47:40 -0300

    add default params

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
 
@@ -6,7 +9,7 @@ port = process.env.PORT || 80
 
 app.use(express.json())
 
-function getRandomArbitrary(min = 0, max = 100) {
+function getRandomArbitrary(min, max) {
   return parseInt(Math.random() * (max - min) + min);
 }
 
@@ -14,10 +17,12 @@ app.get('/', (req, res) => {
   const { max, min, limit } = req.query
 
   const data = []
-  const _limit = limit || 10
+  const _limit = limit ? parseInt(limit) : 10
+  const _min = min ? parseInt(min) : 0
+  const _max = max ? parseInt(max) : 100
 
   for (let i = 0; i < _limit; i++) {
-    const ran = getRandomArbitrary(parseInt(min), parseInt(max))
+    const ran = getRandomArbitrary(_min, _max)
     data.push(ran.toString())
   }
 
